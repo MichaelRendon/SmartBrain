cef1cf7e7e1792aee07d5d5cad7593fb45926ac2
MichaelRendon <49463393+MichaelRendon@users.noreply.github.com>
2019-05-31 17:14:13 -0400

    added Rank component, particles, and form css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import React from 'react';
+import Particles from 'react-particles-js';
 import Navigation from './Components/Navigation/Navigation';
 import Logo from './Components/Logo/Logo';
 import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm.js';
+import Rank from './Components/Rank/Rank.js';
 import './App.css';
+
+const particlesOptions = {
+    particles: {
+    	number: {
+    		value:60,
+    	density: {
+    		enable: true,
+    		value_area: 800
+            }
+        }
+    }
+}
+            
 
 function App() {
   return (
     <div className="App">
+      <Particles className='particles' 
+              params={particlesOptions}
+            />
       <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
       {/*<FaceRecognition />*/}
     </div>
--- src/Components/ImageLinkForm/ImageLinkForm.js
+++ src/Components/ImageLinkForm/ImageLinkForm.js
@@ -1,16 +1,19 @@
 import React from 'react';
+import './imageLinkForm.css';
 
 const ImageLinkForm = () => {
 	return (
-		<div>
+	  <div>
 		<p className='f3'>
 		  {'This Magic Brain will detect faces in your photo.  Give it a try!!'}
 		</p>
-		<div>
-		<input className='f4 pa2 w-70 center' type='text' />
-		<button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>Detect</button>
+		<div className='center'>
+		  <div className='center form pa4 br3 shadow-5'>
+		    <input className='f4 pa2 w-70 center' type='text' />
+		    <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>Detect</button>
+		  </div>
 		</div>
-		</div>
+	  </div>
 	);
 }
 
